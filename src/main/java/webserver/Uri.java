6fd4ea21d94f6229f0c696f84ae78870611400ce
shimbaa <shim9597@gmail.com>
2024-03-14 11:52:03 +0900

    요청 uri 로 부터 지정된 uri를 찾아서 반환한다
    지정된 uri가 없을 시 "DATA_OR_NONE" 을 반환한다
    "DATA_OR_NONE"은
    1) css, 이미지 파일 등의 요청이 오는 경우
    2) 유효하지 않은 uri 값이 들어오는 경우
    두가지의 경우를 표현하기 위한 의도로 만들었다.

---
+++ src/main/java/webserver/Uri.java
@@ -0,0 +1,25 @@
+package webserver;
+
+import java.util.Arrays;
+
+public enum Uri {
+
+    DEFAULT_HOME("/"),
+    INDEX_HTML_HOME("/index.html"),
+    USER_CREATE("/user/create"),
+    DATA_OR_NONE(null);
+
+
+    private final String pattern;
+
+    Uri(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public static Uri from(String requestUri) {
+        return Arrays.stream(values())
+                .filter(uri -> uri.pattern.equals(requestUri))
+                .findAny()
+                .orElse(DATA_OR_NONE);
+    }
+}
