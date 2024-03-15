af3ce1a849e073af1c5ebe911e530b797dca6c48
shimbaa <shim9597@gmail.com>
2024-03-15 15:58:48 +0900

    login 시 호출되는 클래스 구현
    실행 동작은 추후 구현해볼 예정

---
+++ src/main/java/webserver/subhandlers/LoginFormHandler.java
@@ -0,0 +1,13 @@
+package webserver.subhandlers;
+
+import java.io.IOException;
+import webserver.HttpRequest;
+import webserver.HttpResponse;
+
+public class LoginFormHandler implements WebHandler{
+
+    @Override
+    public void process(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
+
+    }
+}
