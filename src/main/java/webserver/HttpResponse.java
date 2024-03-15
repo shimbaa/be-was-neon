c2a3246a7cc00966f4191eb3563724b7e56374a6
shimbaa <shim9597@gmail.com>
2024-03-15 15:00:32 +0900

    HttpResponse 클래스 구현
    http 응답 관련 중복되는 코드를 모아놓음

--- src/main/java/webserver/HttpResponse.java
+++ src/main/java/webserver/HttpResponse.java
@@ -1,10 +1,51 @@
 package webserver;
 
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HttpResponse {
     private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
 
+    private final DataOutputStream dos;
+
+    private HttpResponse(OutputStream out) {
+        this.dos = new DataOutputStream(out);
+    }
+    public static HttpResponse create(OutputStream out) {
+        return new HttpResponse(out);
+    }
+
+    public void response200HeaderByType(int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void responseBody(byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void responseRedirection(String redirectionCode, String location) {
+        try {
+            dos.writeBytes("HTTP/1.1 " + redirectionCode + " \r\n");
+            dos.writeBytes("Location: " + location + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
 }
