8492fc5f600bea3c2ca5277959978c6a0fea915c
shimbaa <shim9597@gmail.com>
2024-03-19 10:20:13 +0900

    변경 예정 사항들에 대한 주석 메모

--- src/main/java/webserver/Uri.java
+++ src/main/java/webserver/Uri.java
@@ -4,9 +4,18 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum Uri {
+    /**
+     * 변경 : / 으로 요청이 들어오면 resources/static/index.html 로 처리되도록 하자
+     */
     DEFAULT_HOME("/"),
+    /**
+     * 변경 : /registration 으로 요청이 들어오면 resources/static/registration/index.html 로 처리되도록 하자
+     */
     USER_CREATE_FORM("/registration"),
     USER_CREATE("/user/create"),
+    /**
+     * 변경 : /login 으로 요청이 들어오면 resources/static/login/index.html 로 처리되도록 하자
+     */
     LOGIN_FORM("/login"),
     DATA("TEMPORAL_VALUE");
 
