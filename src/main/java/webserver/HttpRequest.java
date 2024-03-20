3cfe6d2d7b0a9df18a0e10d07a83396850139ae0
shimbaa <shim9597@gmail.com>
2024-03-20 16:55:00 +0900

    Post 메서드로 넘어온 http 요청 처리 할 수 있도록 코드 변경

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -6,49 +6,101 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HttpRequest {
     private static final Logger logger = LoggerFactory.getLogger(HttpRequest.class);
-
     private final String startLine;
+    private List<String> header;
+    private String body;
 
-    private final String header;
-
-    private final List<String> body;
-
-    public HttpRequest(String startLine, String header, List<String> body) {
-        this.startLine = startLine;
-        this.header = header;
-        this.body = body;
-    }
-
-    public static HttpRequest create(InputStream in) throws IOException {
+    public HttpRequest(InputStream in) throws IOException {
         // todo
         // 코드 정리
         BufferedReader br = new BufferedReader(new InputStreamReader(in, "utf-8"));
-        String startLine = br.readLine();
-        String header = br.readLine();
+        this.startLine = br.readLine();
 
-        logger.debug("start line : {}", startLine);
-        logger.debug("header : {}", header);
+        if (getRequestMethod().equals("GET")) {
+            parseGetRequest(br);
+        }
 
-        List<String> body = new ArrayList<>();
+        if (getRequestMethod().equals("POST")) {
+            logger.debug("POST startLine : {}", startLine);
+            setPostHeader(br);
+            setPostBody(br);
+        }
+    }
+
+    public String getRequestMethod() {
+        String[] tokens = startLine.split(" ");
+        return tokens[0];
+    }
+
+    public String getRequestUri() {
+        String[] tokens = startLine.split(" ");
+        return tokens[1];
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    private void setPostBody(BufferedReader br) throws IOException {
+        int contentLength = 0;
+        Optional<String> any = header.stream()
+                .filter(line -> line.startsWith("Content-Length"))
+                .findAny();
+
+        if (any.isPresent()) {
+            String[] split = any.get().split(" ");
+            contentLength = Integer.parseInt(split[1]);
+        }
+
+        char[] body = new char[contentLength];
+        br.read(body);
+
+        StringBuilder sb = new StringBuilder();
+        for (char each : body) {
+            sb.append(each);
+        }
+        this.body = sb.toString();
+    }
+
+    private void setPostHeader(BufferedReader br) throws IOException {
+        List<String> header = new ArrayList<>();
         String line;
         do {
             line = br.readLine();
-            body.add(line);
-        } while (!line.equals(""));
-        for (String s : body) {
-            logger.debug("body : {}", s);
-        }
+            header.add(line);
+        } while (!line.isEmpty());
+        this.header = header;
 
-        return new HttpRequest(startLine, header, body);
+        printHeaderLog(header);
     }
 
-    public String getUri() {
-        String[] tokens = startLine.split(" ");
-        return tokens[1];
+    private void printHeaderLog(List<String> header) {
+        for (String s : header) {
+            logger.debug("header line : {}", s);
+        }
+    }
+
+    private void parseGetRequest(BufferedReader br) throws IOException {
+        List<String> header = new ArrayList<>();
+        String line;
+        do {
+            line = br.readLine();
+            header.add(line);
+        } while (!line.equals(""));
+        this.header = header;
+    }
+
+    @Override
+    public String toString() {
+        return "startLine='" + startLine + '\'' +
+                ", header='" + header + '\'' +
+                ", body=" + body +
+                '}';
     }
 }
