cd6a4e763dec05b2b9b41f07dd4ef5c8341770b3
shimbaa <shim9597@gmail.com>
2024-03-20 16:53:51 +0900

    post 메서드로 넘어온 html body 내용을 파싱하는 코드

--- src/main/java/webserver/subhandlers/UserCreateHandler.java
+++ src/main/java/webserver/subhandlers/UserCreateHandler.java
@@ -6,7 +6,6 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,12 +19,8 @@ public class UserCreateHandler implements WebHandler {
     public void process(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
         Map<String, String> parameters = new HashMap<>();
 
-        parseQueryString(httpRequest, parameters);
-
-        //파싱 확인용 코드
-        for (Entry<String, String> entry : parameters.entrySet()) {
-            logger.debug("key : {}, value : {}", entry.getKey(), entry.getValue());
-        }
+        String body = httpRequest.getBody();
+        parsePostBody(body, parameters);
 
         User user = new User(parameters.get("userId"), parameters.get("password"), parameters.get("name"),
                 parameters.get("email"));
@@ -36,16 +31,25 @@ public class UserCreateHandler implements WebHandler {
 
         logger.debug("added user in DB : {}", Database.findUserById(user.getUserId()));
 
-        /**
-         * 응답 로직 : redirect to home
-         */
-        httpResponse.responseRedirection("302", "/login/index.html");
+        httpResponse.responseRedirection("302", "/index.html");
     }
 
-    private void parseQueryString(HttpRequest httpRequest, Map<String, String> parameters)
+//    private void parseQueryString(HttpRequest httpRequest, Map<String, String> parameters)
+//            throws UnsupportedEncodingException {
+//        String requestUri = httpRequest.getRequestUri();
+//        String queryString = requestUri.replaceAll("/user/create\\?", "");
+//        String[] pairs = queryString.split("&");
+//        for (String pair : pairs) {
+//            int idx = pair.indexOf("=");
+//            String key = URLDecoder.decode(pair.substring(0, idx), "UTF-8");
+//            String value = URLDecoder.decode(pair.substring(idx + 1), "UTF-8");
+//            parameters.put(key, value);
+//        }
+//    }
+
+    private void parsePostBody(String body, Map<String, String> parameters)
             throws UnsupportedEncodingException {
-        String requestUri = httpRequest.getUri();
-        String queryString = requestUri.replaceAll("/user/create\\?", "");
+        String queryString = body.replaceAll("/user/create\\?", "");
         String[] pairs = queryString.split("&");
         for (String pair : pairs) {
             int idx = pair.indexOf("=");
