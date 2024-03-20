72b3e91937a1da035249c4888d81f0cdb02d367a
shimbaa <shim9597@gmail.com>
2024-03-20 19:35:18 +0900

    userId 가 존재 하지 않는 경우를 고려 하여 Optional 을 적용

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -1,5 +1,6 @@
 package db;
 
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import model.User;
 
@@ -7,14 +8,14 @@ import java.util.Collection;
 import java.util.Map;
 
 public class Database {
-    private static Map<String, User> users = new ConcurrentHashMap<>();
+    private static final Map<String, User> users = new ConcurrentHashMap<>();
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
     }
 
-    public static User findUserById(String userId) {
-        return users.get(userId);
+    public static Optional<User> findUserById(String userId) {
+        return Optional.ofNullable(users.get(userId));
     }
 
     public static Collection<User> findAll() {
