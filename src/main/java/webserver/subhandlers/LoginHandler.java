319a6b6ea7f89cd50549a115ceec48314b4d25f9
shimbaa <shim9597@gmail.com>
2024-03-20 19:55:21 +0900

    로그인 처리 handler 구현
    요청 받은 id 값을 DataBase 에서 조회
    1-1) 해당 id에 해당하는 회원이 존재하고 비밀번호가 일치 : 로그인 성공
    1-2) 비밀번호가 불일치 : 로그인 실패
    2) 해당 id에 해당하는 회원이 존재하지 않음 : 로그인 실패

---
+++ src/main/java/webserver/subhandlers/LoginHandler.java
@@ -0,0 +1,62 @@
+package webserver.subhandlers;
+
+import db.Database;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.HttpRequest;
+import webserver.HttpResponse;
+
+public class LoginHandler implements WebHandler{
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginHandler.class);
+
+    @Override
+    public void process(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
+
+        String body = httpRequest.getBody();
+        Map<String, String> postBodyMap = parsePostBody(body);
+        String userId = postBodyMap.get("userId");
+        String password = postBodyMap.get("password");
+
+        Optional<User> findUser = Database.findUserById(userId);
+
+        if (findUser.isPresent()) { // 해당 id 회원 존재
+            User user = findUser.get();
+            if (isPasswordEquals(password, user)) { // 로그인 성공
+                httpResponse.responseRedirection("302", "/");
+            }
+            if (!isPasswordEquals(password, user)) { //비밀번호 불일치
+                httpResponse.responseRedirection("302", "/login/failed.html");
+            }
+        }
+        if (findUser.isEmpty()) { // 해당 id 회원 존재 x
+            httpResponse.responseRedirection("302", "/login/failed.html");
+        }
+    }
+
+    private boolean isPasswordEquals(String password, User user) {
+        return user.getPassword().equals(password);
+    }
+
+    private Map<String, String> parsePostBody(String body)
+            throws UnsupportedEncodingException {
+        Map<String, String> parameters = new HashMap<>();
+
+        String queryString = body.replaceAll("/user/login\\?", "");
+        String[] pairs = queryString.split("&");
+        for (String pair : pairs) {
+            int idx = pair.indexOf("=");
+            String key = URLDecoder.decode(pair.substring(0, idx), "UTF-8");
+            String value = URLDecoder.decode(pair.substring(idx + 1), "UTF-8");
+            parameters.put(key, value);
+        }
+        return parameters;
+    }
+}
